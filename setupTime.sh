0040a6314ee8ecc7ac7120442e97bae4bb6471ab
Gurjit Singh <g.s@outlook.in>
2022-06-11 23:10:18 +0530

    updts

--- setupTime.sh
+++ setupTime.sh
@@ -1,9 +1,26 @@
-sudo apt-get install ntp
+
+sudo apt-get install systemd-timesyncd
+
+confFile="/etc/systemd/timesyncd.conf"
+
+echo -e "NTP=0.pool.ntp.org\nFallbackNTP=time.cloudflare.com" \
+ | sudo tee -a ${confFile}
+
+# echo "PollIntervalMinSec=900,PollIntervalMaxSec=3600" \
+#  | sudo tee -a ${confFile}
+
+cat ${confFile}
+
+sudo systemctl restart systemd-timesyncd
+
+sudo timedatectl set-ntp true
 
 sudo timedatectl set-timezone Asia/Kolkata
 
-sudo timedatectl set-local-rtc 1
+timedatectl status
 
-sudo ntpd -qg && sudo hwclock -w
+timedatectl timesync-status
 
-sudo systemctl disable systemd-timesyncd --now
+# sudo systemctl disable systemd-timesyncd --now
+# sudo apt-get install ntp
+# sudo ntpd -qg && sudo hwclock -w
