e075cb43467cd36ef3404779249a558528df2996
Gurjit Singh <g.s@outlook.in>
2022-06-11 23:36:04 +0530

    added7z

--- InstallThirdParty.sh
+++ InstallThirdParty.sh
@@ -1,4 +1,9 @@
 
+# Official 7z build 7zz(dynamically linked) or 7zzs(statically linked)
+cd ~/bin && wget -qO - https://www.7-zip.org/a/7z2107-linux-x64.tar.xz |
+tar -Jxf - 7zz && mv ./7zz ./7z && cd ..
+
+# Sublime Text 3
 echo "Installing Sublime Text 3 from official SublimeHQ repo"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg\
  | sudo apt-key add -
@@ -7,8 +12,12 @@ echo "deb https://download.sublimetext.com/ apt/stable/"\
 sudo apt-get update
 sudo apt-get install sublime-text
 
+# NodeJs
 echo "Installing NodeJs from NodeSource"
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt-get install -y nodejs
 # curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 # sudo apt-get install -y nodejs
+
+
+
